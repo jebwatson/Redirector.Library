b0afc573371a2af4a5d34cca5b86a1adbca50501
Jeb Watson <jeb.watson@gmail.com>
2023-04-08 15:56:43 -0500

    Add todos

--- RedirectionService.cs
+++ RedirectionService.cs
@@ -6,7 +6,11 @@ public class RedirectionService
     {
         try
         {
-            Console.WriteLine($"Redirecting from {redirect.RedirectUrl} to {redirect.TargetUrl}...");   
+            Console.WriteLine($"Redirecting from {redirect.RedirectUrl} to {redirect.TargetUrl}...");
+            // TODO: Perform redirect
+            // TODO: Cache results
+            // TODO: Create configurable cache
+            // TODO: Log results
         }
         catch (System.Exception e)
         {
